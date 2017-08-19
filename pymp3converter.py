907838e24df2ddfc2badf7b1793389cdcd6e710d
olezt <lfants666@gmail.com>
2017-08-20 01:33:38 +0300

    Split to multiple functions
    -Also delete temp folderafter finish

--- pymp3converter.py
+++ pymp3converter.py
@@ -1,15 +1,32 @@
 import moviepy.editor as mp
 from pytube import YouTube
 import sys
+import os
+import shutil
 
-if sys.argv[1]:
-    yt = YouTube(sys.argv[1])
+def createTempFolder(tempFolderName):
+        shutil.rmtree(tempFolderName)
+        os.mkdir(tempFolderName)
+
+def downloadVideo(url, tempFolderName):
+    yt = YouTube(url)
     filename = yt.filename
-    extension = yt.videos[0].extension
     yt.set_filename('tempVideo')
-    yt.videos[0].download('temp/')
-    clip = mp.VideoFileClip("temp/tempVideo." + extension)
-    clip.audio.write_audiofile("exported/" + filename + ".mp3")
-else:
-    print ("Please enter a youtube url as argument")
+    yt.videos[0].download(tempFolderName)
+    return {'filename' : filename, 'extension' : yt.videos[0].extension}
 
+def videoToMp3(tempFolderName, extension, filename):
+        clip = mp.VideoFileClip(tempFolderName + "tempVideo." + extension)
+        clip.audio.write_audiofile("exported/" + filename  + ".mp3")
+
+def main():
+        tempFolderName = 'temp/'
+        if sys.argv[1]:
+                createTempFolder(tempFolderName)
+                videoResult = downloadVideo(sys.argv[1], tempFolderName)
+                videoToMp3(tempFolderName, videoResult['extension'], videoResult['filename'])
+                shutil.rmtree(tempFolderName)
+        else:
+                print ("Please enter a youtube url as argument")
+
+main()
