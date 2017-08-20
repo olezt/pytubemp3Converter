30c9012da8bca12e9cc87d48070ff2ce6b05f20e
olezt <lfants666@gmail.com>
2017-08-20 16:51:44 +0300

    Introduce quality argument identifier

--- pymp3converter.py
+++ pymp3converter.py
@@ -15,20 +15,21 @@ def createNewTempFolder(tempFolderName):
     deleteTempFolder(tempFolderName)
     os.mkdir(tempFolderName)
 
-def downloadVideo(url, tempFolderName, filenamePrefix):
+def downloadVideo(url, tempFolderName, filenamePrefix, videoQuality):
+    print (videoQuality)
     yt = YouTube(url)
     filename = yt.filename
     yt.set_filename(filenamePrefix + 'tempVideo')
-    yt.videos[0].download(tempFolderName)
-    return {'filename' : filename, 'extension' : yt.videos[0].extension}
+    yt.videos[videoQuality].download(tempFolderName)
+    return {'filename' : filename, 'extension' : yt.videos[videoQuality].extension}
 
 def videoToMp3(tempFolderName, extension, filename, filenamePrefix):
     id = id_generator()
     clip = mp.VideoFileClip(tempFolderName + filenamePrefix + "tempVideo." + extension)
     clip.audio.write_audiofile("exported/" + filename + " " + id  + ".mp3")
 
-def downloadAndConvert(url, tempFolderName, filenamePrefix):
-    videoResult = downloadVideo(url, tempFolderName, filenamePrefix)
+def downloadAndConvert(url, tempFolderName, filenamePrefix, videoQuality):
+    videoResult = downloadVideo(url, tempFolderName, filenamePrefix, videoQuality)
     videoToMp3(tempFolderName, videoResult['extension'], videoResult['filename'], filenamePrefix)
 
 def readFile(filename):
@@ -39,16 +40,17 @@ def main():
     tempFolderName = 'temp/'
     deleteTempFolder(tempFolderName)
     createNewTempFolder(tempFolderName)
-    if len(sys.argv)==3 and sys.argv[1]=='-u':
+    videoQuality = 0 if (len(sys.argv)>3 and sys.argv[3]=='-lq') else -1
+    if len(sys.argv)>=3 and sys.argv[1]=='-u':
         url = sys.argv[2]
-        downloadAndConvert(url, tempFolderName, '')
-    elif len(sys.argv)==3 and sys.argv[1]=='-f':
+        downloadAndConvert(url, tempFolderName, '', videoQuality)
+    elif len(sys.argv)>=3 and sys.argv[1]=='-f':
         filename = sys.argv[2]
         urls = readFile(filename)
         filenamePrefix = 0 
         for url in urls:
             filenamePrefix += 1
-            downloadAndConvert(url, tempFolderName, str(filenamePrefix))
+            downloadAndConvert(url, tempFolderName, str(filenamePrefix), videoQuality)
     else:
         print ("Please use one of the available arguments: " + str(avArgvs))
     deleteTempFolder(tempFolderName)
