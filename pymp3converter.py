88c1dc18647689e5e5561ca489213404de9b2f38
olezt <lfants666@gmail.com>
2017-08-20 16:42:21 +0300

    Introduce file input functionality

--- pymp3converter.py
+++ pymp3converter.py
@@ -15,27 +15,42 @@ def createNewTempFolder(tempFolderName):
     deleteTempFolder(tempFolderName)
     os.mkdir(tempFolderName)
 
-def downloadVideo(url, tempFolderName):
+def downloadVideo(url, tempFolderName, filenamePrefix):
     yt = YouTube(url)
     filename = yt.filename
-    yt.set_filename('tempVideo')
+    yt.set_filename(filenamePrefix + 'tempVideo')
     yt.videos[0].download(tempFolderName)
     return {'filename' : filename, 'extension' : yt.videos[0].extension}
 
-def videoToMp3(tempFolderName, extension, filename):
+def videoToMp3(tempFolderName, extension, filename, filenamePrefix):
     id = id_generator()
-    clip = mp.VideoFileClip(tempFolderName + "tempVideo." + extension)
+    clip = mp.VideoFileClip(tempFolderName + filenamePrefix + "tempVideo." + extension)
     clip.audio.write_audiofile("exported/" + filename + " " + id  + ".mp3")
 
+def downloadAndConvert(url, tempFolderName, filenamePrefix):
+    videoResult = downloadVideo(url, tempFolderName, filenamePrefix)
+    videoToMp3(tempFolderName, videoResult['extension'], videoResult['filename'], filenamePrefix)
+
+def readFile(filename):
+    return tuple(open(filename, 'r'))
+
 def main():
+    avArgvs = ['-u', '-f']
     tempFolderName = 'temp/'
-    if sys.argv[1]:
-        deleteTempFolder(tempFolderName)
-        createNewTempFolder(tempFolderName)
-        videoResult = downloadVideo(sys.argv[1], tempFolderName)
-        videoToMp3(tempFolderName, videoResult['extension'], videoResult['filename'])
-        deleteTempFolder(tempFolderName)
+    deleteTempFolder(tempFolderName)
+    createNewTempFolder(tempFolderName)
+    if len(sys.argv)==3 and sys.argv[1]=='-u':
+        url = sys.argv[2]
+        downloadAndConvert(url, tempFolderName, '')
+    elif len(sys.argv)==3 and sys.argv[1]=='-f':
+        filename = sys.argv[2]
+        urls = readFile(filename)
+        filenamePrefix = 0 
+        for url in urls:
+            filenamePrefix += 1
+            downloadAndConvert(url, tempFolderName, str(filenamePrefix))
     else:
-        print ("Please enter a youtube url as argument")
+        print ("Please use one of the available arguments: " + str(avArgvs))
+    deleteTempFolder(tempFolderName)
 
 main()
